4ffb3ab0c1d98fcedbf566997c6576d4249d1590
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-03-18 21:37:05 +0700

    cài đặt mongoose

---
+++ models/products.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    { 
+        title: String,
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        stock: Number,
+        thumbnail: Number,
+        status: String,
+        position: Number,
+        deleted: Boolean
+    }
+);
+const Product = mongoose.model('Product', productSchema,"products"); // products sau giống phẩy thứ 3 là tên collection trong database
+
+module.exports = Products;
