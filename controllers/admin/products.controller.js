533cad5f3a0d992337602338e6baf06e7da95727
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-04-03 15:18:23 +0700

    làm giao diện sản phẩm admin

---
+++ controllers/admin/products.controller.js
@@ -0,0 +1,5 @@
+module.exports.products = ((req,res)=>{
+    res.render("admin/pages/products/index.pug",{
+        pageTitle: "Trang sản phẩm"
+    });
+})
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,8 +1,10 @@
-const dashboardRoutes = require("./dashboard.route");  
+const dashboardRoutes = require("./dashboard.route.js"); 
+const productRoutes = require("./products.route.js"); 
 const systemConfig = require("../../config/system")
 module.exports = (app) =>{
    const PATH_ADMIN = systemConfig.prefixAdmin;
    app.use(PATH_ADMIN+"/dashboard",dashboardRoutes); 
-    
+   app.use(PATH_ADMIN+"/product",productRoutes);
 
 }
+
