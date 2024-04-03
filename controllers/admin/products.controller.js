19e6ca89c8778ed31fa1b598da90214e81ec3319
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-04-03 15:25:08 +0700

    thêm ghi chú cho controller

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,3 +1,4 @@
+// [GET] /admin/product
 module.exports.products = ((req,res)=>{
     res.render("admin/pages/products/index.pug",{
         pageTitle: "Trang sản phẩm"
--- controllers/client/controller.products.js
+++ controllers/client/controller.products.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/products.model");
+// [GET] /product
 
 module.exports.index = async (req,res)=>{
     const products = await Product.find({
