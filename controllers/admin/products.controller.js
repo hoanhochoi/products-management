8ce48f7299cb7d77590fe993843e660a45f6d1d3
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-04-03 15:49:01 +0700

    lấy data in ra sản phẩm admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,12 @@
+const Product = require("../../models/products.model");
 // [GET] /admin/product
-module.exports.products = ((req,res)=>{
+module.exports.products = async (req,res)=>{
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products);
     res.render("admin/pages/products/index.pug",{
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products : products
     });
-})
+}
