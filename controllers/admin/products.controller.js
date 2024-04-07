857eb56d1c4217d12cdbcba605c02e9634a1dc52
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-04-07 22:13:20 +0700

    tối ưu bộ lọc

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,40 +1,22 @@
 const Product = require("../../models/products.model");
+const filterStatusHelper = require("../../helpers/filterStatusHelper")
 // [GET] /admin/product
 module.exports.products = async (req,res)=>{
-    let filterStatus = [
-        {
-            name : "Tất cả",
-            status : "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name:"Dừng hoạt động",
-            status:"inactive",
-            class:""
-        }
-    ]
+ 
+    // đoạn code bộ lọc
+
+    let filterStatus = filterStatusHelper(req.query)
+    console.log(filterStatus);
 
     let find = {
         deleted: false
-    }
-
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    }else{
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
     }
 
     if(req.query.status) // câu lệnh nếu thêm pram vào sẽ lấy data
     {
         find.status = req.query.status;
     }
+
     let keyword = "";
 
     if(req.query.keyword){
@@ -46,7 +28,7 @@ module.exports.products = async (req,res)=>{
     }
 
     const products = await Product.find(find)
-    console.log(products);
+    // console.log(products);
     res.render("admin/pages/products/index.pug",{
         pageTitle: "Trang sản phẩm",
         products : products,
