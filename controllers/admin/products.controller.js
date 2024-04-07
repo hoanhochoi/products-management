035600bdbfb2f2b68bc9446a06c4a8caf9fca008
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-04-07 21:27:17 +0700

    tính năng tìm kiếm(phần 2)

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -35,12 +35,22 @@ module.exports.products = async (req,res)=>{
     {
         find.status = req.query.status;
     }
+    let keyword = "";
+
+    if(req.query.keyword){
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword,"i"); // sử dụng regex trong javascript và tham số thứ hai là i là không phân biệt chữ hoa chữ thường
+        find.title = regex; // monggo hỗ trợ tìm regex
+        
+    }
 
     const products = await Product.find(find)
     console.log(products);
     res.render("admin/pages/products/index.pug",{
         pageTitle: "Trang sản phẩm",
         products : products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword : keyword
     });
 }
