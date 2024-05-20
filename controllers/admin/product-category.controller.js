793a760415670e26eeeb615bcd63ee5d882cc5f8
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-05-20 22:02:10 +0700

    phần quản lý danh mục sản phẩm(phần 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,23 +1,37 @@
 const ProductCategory = require("../../models/products-category.model");
 const systemConfig = require("../../config/system")
+const createTreeHelper = require("../../helpers/createTreeHelper");
 
 // [GET] admin/product-category
 module.exports.index = async (req,res)=>{
     const find = {
         deleted: false
-    }
-    const records = await ProductCategory.find(find);
+    };
+
+    
+
+    const record = await ProductCategory.find(find);
+    const newRecord = createTreeHelper.tree(record);
+
     res.render("./admin/pages/products-category/index.pug",{
         pageTitle:"Trang danh mục sản phẩm",
-        records: records
+        records: newRecord
 
     })
 }
 
 // [GET] admin/product-category/create
 module.exports.create = async (req,res)=>{
+    const find = {
+        deleted: false
+    };
+   
+    const record = await ProductCategory.find(find);
+    const newRecord = createTreeHelper.tree(record);
+    console.log(newRecord);
     res.render("./admin/pages/products-category/create.pug",{
         pageTitle: "Trang tạo danh mục sản phẩm",
+        record: newRecord
     })
 }
 
@@ -31,8 +45,9 @@ module.exports.createPost = async (req,res) =>{
     }
     const record = new ProductCategory(req.body);
     record.save();
-    console.log(req.body);
+    // console.log(req.body);
 
     res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+    
     
 }
---
+++ helpers/createTreeHelper.js
@@ -0,0 +1,21 @@
+function createTree(arr,parentId = ""){
+    const tree = [];
+
+    arr.forEach( (item)=>{
+
+        if(item.parent_id === parentId){
+            const newItem = item;
+            const children = createTree(arr,item.id);
+            if(children.length > 0){
+                newItem.children = children;
+            }
+            tree.push(newItem);
+        } 
+    });
+    return tree; // đoạn này mở dev tool mới hiển thị hết data còn terminal nhiều lúc không hiện hết
+}
+
+module.exports.tree = (arr,parentId = "")=>{
+    const tree = createTree(arr,parentId= "");
+    return tree;
+}
