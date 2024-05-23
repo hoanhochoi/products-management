a1460d05b0f10a9687fd96f8d0f3940b0e39ddad
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-05-23 20:17:11 +0700

    xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/roles.controller.js
+++ controllers/admin/roles.controller.js
@@ -1,7 +1,8 @@
 const Role = require("../../models/roles.model");
 const systemConfig = require("../../config/system");
 const system = require("../../config/system");
-// [GET] /admin/product/roles 
+const { findOne } = require("../../models/products.model");
+// [GET] /admin/roles 
 module.exports.index = async (req,res)=>{
     let find = {
         deleted: false
@@ -13,7 +14,7 @@ module.exports.index = async (req,res)=>{
     })
 
 }
-// [GET] /admin/product/roles/create
+// [GET] /admin/roles/create
 module.exports.create = async  (req,res)=>{
     let find = {
         deleted: false
@@ -24,9 +25,39 @@ module.exports.create = async  (req,res)=>{
     })
 }
 
-// [POST] /admin/product/roles/create
+// [POST] /admin/roles/create
 module.exports.createPost = async  (req,res)=>{
    const record = new Role(req.body);
    record.save();
    res.redirect(`${systemConfig.prefixAdmin}/roles`)
+}
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req,res)=>{
+   try {
+    const id = req.params.id;
+    const find = {
+        _id: id,
+        deleted:false
+    }
+    const record = await Role.findOne(find);
+    res.render("./admin/pages/roles/edit.pug",{
+        pageTitle: "Chỉnh sửa phân quyền",
+        record: record
+    });
+   } catch (error) {
+    req.flash("error","Không có nhóm quyền này");
+   res.redirect(`${systemConfig.prefixAdmin}/roles`)
+
+}
+}
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req,res)=>{
+    const id = req.params.id;
+    console.log(req.body);
+    await Role.updateOne({_id:id},req.body)
+    req.flash("success","cập nhật phân quyền thành công");
+    res.redirect("back");
+    
 }
--- routes/admin/roles.routes.js
+++ routes/admin/roles.routes.js
@@ -4,6 +4,8 @@ const controller = require("../../controllers/admin/roles.controller.js");
 
 router.get("/",controller.index);
 router.get("/create",controller.create);
-router.post("/create",controller.createPost)
+router.post("/create",controller.createPost);
+router.get("/edit/:id",controller.edit);
+router.patch("/edit/:id",controller.editPatch);
 
 module.exports = router;
