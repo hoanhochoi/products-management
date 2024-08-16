771ff9acbcd1428c92f5b16272f9b6bae62b2d30
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-08-16 22:25:49 +0700

    nút mini cart trên header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -9,6 +9,11 @@ module.exports.cart = async (req,res,next)=>{
         res.cookie("cartId",cart.id,{expires: new Date(Date.now() + expiresCookie)}); 
     }else{
         // lấy ra thôi
+        const cart = await Cart.findOne({
+            _id : req.cookies.cartId
+        })
+        cart.totalQuantity = cart.products.reduce((sum,item)=> sum + item.quantity,0);
+        res.locals.miniCart = cart;
     }
     next();
 }
