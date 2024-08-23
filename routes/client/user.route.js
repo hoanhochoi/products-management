ea51291bb4e95bc7064373ae4099e6bf0997a5c6
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-08-23 22:07:57 +0700

    đăng xuất

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -8,5 +8,5 @@ route.post("/register",validate.registerPost,controller.registerPost)
 
 route.get("/login",controller.login)
 route.post("/login",validate.loginPost,controller.loginPost)
-
+route.get("/logout",controller.logout);
 module.exports = route;
