dfb8f7e38afb5217a58c8fe2202bcd030b69047e
Trần Đức Hoàn <duchoanlc4@gmail.com>
2024-09-05 14:56:11 +0700

    chèn icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,12 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 // CLIENT_SEND_MESAGE
 const formSendData = document.querySelector(".chat .inner-form")
-if(formSendData){
-    formSendData.addEventListener("submit",(e)=>{
+if (formSendData) {
+    formSendData.addEventListener("submit", (e) => {
         e.preventDefault();
         const content = e.target.elements.content.value;
-        if(content){
-            socket.emit("CLIENT_SEND_MESSAGE",content); // để gửi đi có biến là content
+        if (content) {
+            socket.emit("CLIENT_SEND_MESSAGE", content); // để gửi đi có biến là content
             e.target.elements.content.value = "";
         }
     })
@@ -14,16 +15,16 @@ if(formSendData){
 // END CLIENT_SEND_MESAGE
 
 // SERVER_RETURN_MESSAGE
-socket.on("SERVER_RETURN_MESSAGE",(data)=>{
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
     console.log(data);
     const myId = document.querySelector("[my-id]").getAttribute("my-id");
     const body = document.querySelector(".chat .inner-body");
     const divNew = document.createElement("div");
     let fullNameHTML = ""
-    if(myId == data.userId){
+    if (myId == data.userId) {
         divNew.classList.add("inner-outgoing");
 
-    }else{
+    } else {
         divNew.classList.add("inner-incoming");
         fullNameHTML = `<div class="inner-name">${data.fullName}</div>`
     }
@@ -39,8 +40,35 @@ socket.on("SERVER_RETURN_MESSAGE",(data)=>{
 
 // Scroll chat to bottom
 const bodyScroll = document.querySelector(".chat .inner-body");
-if(bodyScroll){
+if (bodyScroll) {
     bodyScroll.scrollTop = bodyScroll.scrollHeight;
     // khiến thanh scroll luôn ở dưới
 }
 // end Scroll chat to bottom
+
+// show icon chat
+const buttonIcon = document.querySelector('.button-icon')
+if (buttonIcon) {
+    const tooltip = document.querySelector('.tooltip')
+    Popper.createPopper(buttonIcon, tooltip)
+    document.querySelector('.button-icon').onclick = () => {
+        tooltip.classList.toggle('shown')
+    }
+}
+
+// end show icon chat
+
+
+// insert icon
+const emojiPicker = document.querySelector("emoji-picker");
+if(emojiPicker){
+    let input = document.querySelector(".chat .inner-form input[name='content']");
+
+    emojiPicker.addEventListener("emoji-click",(e)=>{
+        
+        console.log(e.detail.unicode);
+        const icon = e.detail.unicode;
+        input.value += icon;
+    })
+}
+// end insert icon
